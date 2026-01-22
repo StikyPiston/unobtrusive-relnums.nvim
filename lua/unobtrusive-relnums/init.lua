3e6501898f86132264c79806551823d732c3fb75
StikyPiston <156162907+StikyPiston@users.noreply.github.com>
2026-01-22 11:33:32 +0200

    feat: Config function, namespace

--- lua/unobtrusive-relnums/init.lua
+++ lua/unobtrusive-relnums/init.lua
@@ -1,3 +1,11 @@
 local M = {}
 
+local ns = vim.api.nvim_create_namespace("unobtrusive-relnums")
+
+local config = {
+	step         = 5,
+	max_distance = 200,
+	sign_hl      = "LineNr"
+}
+
 return M
