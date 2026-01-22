36c9a153e143613e7e792378e890484abaa95b90
StikyPiston <156162907+StikyPiston@users.noreply.github.com>
2026-01-22 16:01:51 +0200

    feat: Change highlight group to comment by default

--- lua/unobtrusive-relnums/init.lua
+++ lua/unobtrusive-relnums/init.lua
@@ -5,7 +5,7 @@ local ns = vim.api.nvim_create_namespace("unobtrusive-relnums")
 local config = {
 	step         = 5,
 	max_distance = 200,
-	sign_hl      = "LineNr"
+	sign_hl      = "Comment"
 }
 
 M.render = function()
